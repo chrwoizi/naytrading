55ec91056989834bc02b3bce69fd7981c183a25a
chrwoizi <christian@woizischke.de>
2017-11-23 13:27:47 +0100

    Run tensorflow in py file from C#

---
+++ StockFlow/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿namespace StockFlow
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Windows;
+
+    using Python.Runtime;
+    
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            ButtonBase_OnClick(null, null);
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            using (Py.GIL())
+            {
+                dynamic learn = Py.Import("learn");
+
+                var x_train = ToPyList(new float[] { 1, 2, 3, 4 });
+                var y_train = ToPyList(new float[] { 0, -1, -2, -3 });
+
+                dynamic result = learn.Learn(x_train, y_train);
+                
+                dynamic curr_W = result[0];
+                dynamic curr_b = result[1];
+                dynamic curr_loss = result[2];
+                this.TextBlock.Text += string.Format("W: {0} b: {1} loss: {2}", curr_W, curr_b, curr_loss);
+            }
+        }
+
+        private static PyList ToPyList(IEnumerable<float> v)
+        {
+            return new PyList(v.Select(x => new PyFloat(x)).Cast<PyObject>().ToArray());
+        }
+    }
+}
---
+++ StockFlow/learn.py
@@ -0,0 +1,32 @@
+﻿import tensorflow as tf
+
+def Learn(x_train, y_train):
+
+	# Model parameters
+	W = tf.Variable([.3], dtype=tf.float32)
+	b = tf.Variable([-.3], dtype=tf.float32)
+
+	# Model input and output
+	x = tf.placeholder(tf.float32)
+	y = tf.placeholder(tf.float32)
+	
+	linear_model = W*x + b
+	
+	# loss
+	loss = tf.reduce_sum(tf.square(linear_model - y)) # sum of the squares
+
+	# optimizer
+	optimizer = tf.train.GradientDescentOptimizer(0.01)
+	train = optimizer.minimize(loss)
+	
+	# training loop
+	init = tf.global_variables_initializer()
+	sess = tf.Session()
+	sess.run(init)
+	for i in range(1000):
+		sess.run(train, {x: x_train, y: y_train})
+	
+	# evaluate training accuracy
+	curr_W, curr_b, curr_loss = sess.run([W, b, loss], {x: x_train, y: y_train})
+	
+	return [curr_W, curr_b, curr_loss]
