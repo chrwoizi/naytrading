3bb8769faa8a7a6c89a251b2a1999ed3b04599f5
chrwoizi <christian@woizischke.de>
2017-12-01 23:54:40 +0100

    Load more instruments. Add show-new button. Add trading decision buttons. Present new snapshots. Remember trading decision. Present old snapshots that are missing a trading decision. Fix list ordering. Set styling and static text of web pages. Remove deprecated code.

--- StockFlow/MainWindow.xaml.cs
+++ StockFlow/MainWindow.xaml.cs
@@ -1,28 +1,16 @@
 ﻿namespace StockFlow
 {
-    using System;
     using System.Collections.Generic;
-    using System.IO;
     using System.Linq;
-    using System.Net;
-    using System.Threading;
     using System.Windows;
 
     using Python.Runtime;
-
-    using StockFlow.Properties;
-
-    using YahooFinanceApi;
-    using System.Net.Http;
-    using Newtonsoft.Json;
 
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        private const string WallstreetOnline = "https://www.***REMOVED***/_rpc/json/instrument/chartdata/loadRawData?q%5BinstId%5D={0}&q%5BmarketId%5D={1}&q%5Bmode%5D=hist&q%5Bts%5D={2}";
-
         public MainWindow()
         {
             InitializeComponent();
@@ -30,12 +18,6 @@
         
         private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
         {
-            GetWallstreetOnline("1105", "21");
-            return;
-
-            GetHistorical();
-            return;
-
             using (Py.GIL())
             {
                 dynamic learn = Py.Import("learn");
@@ -57,101 +39,6 @@
             return new PyList(v.Select(x => new PyFloat(x)).Cast<PyObject>().ToArray());
         }
         
-        public class WallstreetOnlineJson
-        {
-            //public string m { get; set; }
-            //public string markets { get; set; }
-            public decimal? o { get; set; }
-            public decimal? c { get; set; }
-            public long? s { get; set; }
-            public long? i { get; set; }
-            public bool? cached { get; set; }
-            public decimal? decimals { get; set; }
-            public IList<IList<decimal?>> data { get; set; }
-
-            public decimal? GetOpen(int id)
-            {
-                return data[id][1];
-            }
-            public decimal? GetHigh(int id)
-            {
-                return data[id][2];
-            }
-            public decimal? GetLow(int id)
-            {
-                return data[id][3];
-            }
-            public decimal? GetClose(int id)
-            {
-                return data[id][4];
-            }
-            public DateTime GetDate(int id)
-            {
-                return new DateTime(1970,1,1).AddSeconds((long)data[id][5]);
-            }
-        }
-
-        public class Rate
-        {
-            public DateTime Time { get; set; }
-            public decimal? Open { get; set; }
-            public decimal? Close { get; set; }
-            public decimal? High { get; set; }
-            public decimal? Low { get; set; }
-
-            public Rate(IList<decimal?> data)
-            {
-                Open = data[1];
-                Close = data[4];
-                High = data[2];
-                Low = data[3];
-                Time = new DateTime(1970, 1, 1).AddSeconds((long)data[6]);
-            }
-
-            public override string ToString()
-            {
-                return $"{Time} {Open} {Close} {High} {Low}";
-            }
-        }
-
-        private async void GetWallstreetOnline(string instrumentId, string marketId)
-        {
-            var seconds = (int)(DateTime.Now - new DateTime(1970, 1, 1)).TotalSeconds;
-            var url = string.Format(WallstreetOnline, instrumentId, marketId, seconds);
-
-            using (var httpClient = new HttpClient())
-            {
-                var json = await httpClient.GetStringAsync(url);
-                var data = JsonConvert.DeserializeObject<WallstreetOnlineJson>(json);
-                var rates = data.data.Where(x => x.Count == 7).Select(x => new Rate(x)).OrderBy(x => x.Time).ToLookup(x => x.Time, x => x);
-                this.TextBlock.Text += $"last data: {rates.Last().First()}\n";
-            }
-
-            //using (WebClient wc = new WebClient())
-            //{
-            //    var json = wc.DownloadString(url);
-            //}
-        }
-
-        private async void GetHistorical()
-        {
-            WebProxy proxy = null;
-            if (!string.IsNullOrEmpty(Settings.Default.ProxyAddress))
-            {
-                proxy = new WebProxy(new Uri(Settings.Default.ProxyAddress, UriKind.Absolute));
-                proxy.Credentials = new NetworkCredential(Settings.Default.ProxyUser, Settings.Default.ProxyPassword);
-            }
-
-            // You should be able to query data from various markets including US, HK, TW
-            // The startTime & endTime here defaults to EST timezone
-            var history = await Yahoo.GetHistoricalAsync("AAPL", DateTime.Today.AddMonths(-12*5), DateTime.Today.AddDays(-1), Period.Daily, default(CancellationToken), proxy);
-
-            foreach (var candle in history)
-            {
-                this.TextBlock.Text += $"DateTime: {candle.DateTime}, Open: {candle.Open}, High: {candle.High}, Low: {candle.Low}, Close: {candle.Close}, Volume: {candle.Volume}, AdjustedClose: {candle.AdjustedClose}\n";
-            }
-        }
-
         private void Settings_OnClick(object sender, RoutedEventArgs e)
         {
             new SettingsWindow() { DataContext = new SettingsViewModel() }.ShowDialog();
