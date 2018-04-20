718025fd05a757f0fbee19f07373ac3cc9007d6c
chrwoizi <christian@woizischke.de>
2018-04-20 10:23:25 +0200

    Fix upgrade script.

--- StockFlow.Node/upgrade_production.sh
+++ StockFlow.Node/upgrade_production.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 killall production.sh
 killall node
-rm production.sh
-rm upgrade_production.sh
 cd ..
 git pull
 cd StockFlow.Node
-chmod +x production.sh
-chmod +x upgrade_production.sh
 ./production.sh &
