bc26eb94f0ac203b9ccd17c95ee1e3927d7f5680
chrwoizi <christian@woizischke.de>
2018-05-24 20:43:06 +0200

    Pause trading on the weekend.

--- StockFlow.Trader/Program.cs
+++ StockFlow.Trader/Program.cs
@@ -205,6 +205,12 @@ namespace StockFlow.Trader
 
         private static bool IsStockExchangeOpen()
         {
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Saturday)
+                return false;
+
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Sunday)
+                return false;
+
             bool isHoliday = new GermanPublicHoliday().IsPublicHoliday(DateTime.Today);
             if (isHoliday)
             {
