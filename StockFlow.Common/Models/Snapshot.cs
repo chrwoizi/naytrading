f778c78fa25ade4d66601e1758e95c83372cd5f2
chrwoizi <christian@woizischke.de>
2018-05-31 17:09:49 +0200

    Export usersnapshot ID.

--- StockFlow.Common/Models/Snapshot.cs
+++ StockFlow.Common/Models/Snapshot.cs
@@ -6,15 +6,17 @@ namespace StockFlow.Web.Models
 {
     public class Snapshot
     {
-        public int ID { get; set; }
+        public long ID { get; set; }
 
         public DateTime StartTime { get; set; }
         
         public DateTime Time { get; set; }
-        
-        public DateTime ModifiedTime { get; set; }
+
+        public long DecisionId { get; set; }
 
         public string Decision { get; set; }
+
+        public DateTime DecisionTime { get; set; }
 
         public decimal Price { get; set; }
 
