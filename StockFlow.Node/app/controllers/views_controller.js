5399ac584dd0e357db4d8d73103d6c1f7e1fa525
chrwoizi <christian@woizischke.de>
2018-06-04 17:40:32 +0200

    Run processing for all users. Show download links and metadata of processed files on manage page. Fix division by zero in normalization.

--- StockFlow.Node/app/controllers/views_controller.js
+++ StockFlow.Node/app/controllers/views_controller.js
@@ -1,4 +1,7 @@
 var exports = module.exports = {}
+var fs = require('fs');
+var path = require('path');
+var dateFormat = require('dateFormat');
 var config = require('../config/envconfig');
 
 function get_default_args(req) {
@@ -30,9 +33,63 @@ exports.contact = function (req, res) {
 
 }
 
+function formatTime(obj) {
+    var str = obj.time;
+    var date = new Date(Date.UTC(str.substr(0, 4), parseInt(str.substr(4, 2)) - 1, str.substr(6, 2), str.substr(8, 2), parseInt(str.substr(10, 2)), str.substr(12, 2)));
+    obj.timeStr = dateFormat(date, 'dd.mm.yyyy HH:mm:ss');
+}
+
+function formatTestDataRatio(obj) {
+    obj.testDataRatioPercent = (obj.test_data_ratio * 100).toFixed(2);
+}
+
 exports.manage = function (req, res) {
 
-    res.render('manage', get_default_args(req));
+    var args = get_default_args(req);
+
+    var filePath = path.resolve(config.processing_dir + "/" + req.user.email + "/buying_train_aug_norm.csv");
+    if (fs.existsSync(filePath) && fs.existsSync(filePath + ".meta")) {
+        args.buyingTrain = JSON.parse(fs.readFileSync(filePath + ".meta", 'utf8'));
+        formatTime(args.buyingTrain);
+        formatTestDataRatio(args.buyingTrain);
+    }
+    else {
+        args.buyingTrain = null;
+    }
+
+    filePath = path.resolve(config.processing_dir + "/" + req.user.email + "/buying_test_aug_norm.csv");
+    if (fs.existsSync(filePath) && fs.existsSync(filePath + ".meta")) {
+        args.buyingTest = JSON.parse(fs.readFileSync(filePath + ".meta", 'utf8'));
+        formatTime(args.buyingTest);
+        formatTestDataRatio(args.buyingTest);
+    }
+    else {
+        args.buyingTest = null;
+    }
+
+    filePath = path.resolve(config.processing_dir + "/" + req.user.email + "/selling_train_aug_norm.csv");
+    if (fs.existsSync(filePath) && fs.existsSync(filePath + ".meta")) {
+        args.sellingTrain = JSON.parse(fs.readFileSync(filePath + ".meta", 'utf8'));
+        formatTime(args.sellingTrain);
+        formatTestDataRatio(args.sellingTrain);
+    }
+    else {
+        args.sellingTrain = null;
+    }
+
+    filePath = path.resolve(config.processing_dir + "/" + req.user.email + "/selling_test_aug_norm.csv");
+    if (fs.existsSync(filePath) && fs.existsSync(filePath + ".meta")) {
+        args.sellingTest = JSON.parse(fs.readFileSync(filePath + ".meta", 'utf8'));
+        formatTime(args.sellingTest);
+        formatTestDataRatio(args.sellingTest);
+    }
+    else {
+        args.sellingTest = null;
+    }
+
+    args.hasProcessedData = args.buyingTrain != null || args.buyingTest != null || args.sellingTrain != null || args.sellingTest != null;
+
+    res.render('manage', args);
 
 }
 
