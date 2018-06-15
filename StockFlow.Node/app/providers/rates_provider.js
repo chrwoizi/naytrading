ffa6a7b17213cfc44fcc11195a51bc0092f1b8af
chrwoizi <christian@woizischke.de>
2018-06-15 18:27:07 +0000

    Check next market if rates are incomplete.

--- StockFlow.Node/app/providers/rates_provider.js
+++ StockFlow.Node/app/providers/rates_provider.js
@@ -17,13 +17,49 @@ exports.providers = providers;
 exports.market_not_found = "market not found";
 exports.invalid_response = "invalid response";
 
-exports.getRates = async function (source, instrumentId, preferredMarketId, startTime, endTime) {
+exports.getRates = async function (source, instrumentId, preferredMarketId, startTime, endTime, checkRates) {
     if (providers[source]) {
-        var rates = await providers[source].getRates(source, instrumentId, preferredMarketId, startTime, endTime);
-        if (rates) {
-            rates.Source = source;
+
+        var marketIds = providers[source].markets;
+        if (preferredMarketId != null && typeof (preferredMarketId) === 'string' && preferredMarketId.length > 0) {
+
+            var index = marketIds.indexOf(preferredMarketId);
+            if (index > -1) {
+                // remove known market from list
+                marketIds.splice(index, 1);
+            }
+
+            // push known market to front of list
+            marketIds.unshift(preferredMarketId);
         }
-        return rates;
+
+        for (var m = 0; m < marketIds.length; ++m) {
+            var marketId = marketIds[m];
+
+            ratesResponse = await providers[source].getRates(source, instrumentId, marketId, startTime, endTime);
+
+            if (ratesResponse.Rates && ratesResponse.Rates.length > 0 && await checkRates(ratesResponse.Rates)) {
+                ratesResponse.Source = source;
+                ratesResponse.MarketId = marketId;
+                break;
+            }
+
+            if (ratesResponse.MarketIds) {
+                // remove markets that dont exist
+                marketIds = marketIds.filter(x => ratesResponse.MarketIds.indexOf(x) > -1);
+                m = marketIds.indexOf(marketId);
+            }
+
+            // try next market or abort if none left
+            if (m < marketIds.length - 1) {
+                continue;
+            }
+            else {
+                throw exports.market_not_found;
+            }
+        }
+
+        return ratesResponse;
     }
     else {
         return null;
