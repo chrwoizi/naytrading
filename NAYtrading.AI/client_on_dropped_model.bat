23410e919b754e48e061d443bbff62e7d734e673
chrwoizi <christian@woizischke.de>
2018-07-20 17:38:24 +0000

    Refactorings

--- NAYtrading.AI/client_on_dropped_model.bat
+++
@@ -1,2 +0,0 @@
-python client.py --buy_checkpoint_dir=%1\\buy --sell_checkpoint_dir=%1\\sell
-pause
--- NAYtrading.AI/main_buying_train_norm.bat
+++ NAYtrading.AI/main_buying_train_norm.bat
@@ -1,2 +1,6 @@
-python main.py --action=buy --model_dir=buying --train_file=buying_train_norm.csv --test_file=buying_test_norm.csv --repeat_train_data=1 --epochs=60
+pip install numpy
+pip install tensorflow
+pip install tensorflow-gpu
+pip install matplotlib
+python src\main.py --action=buy --model_dir=buying --train_file=buying_train_norm.csv --test_file=buying_test_norm.csv --repeat_train_data=1 --epochs=60
 pause
