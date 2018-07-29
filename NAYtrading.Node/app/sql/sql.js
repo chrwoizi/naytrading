dea76e7aa0122e97054258474bfdd484a49b49f0
chrwoizi <christian@woizischke.de>
2018-07-29 18:41:48 +0000

    Fix time zone issues when using sql and avoid duplicates in portfolios job.

--- NAYtrading.Node/app/sql/sql.js
+++ NAYtrading.Node/app/sql/sql.js
@@ -2,6 +2,7 @@ var exports = module.exports = {}
 var mysql = require('mysql');
 var moment = require('moment');
 var config = require('../config/envconfig');
+var dateFormat = require('dateformat');
 
 config.database.connectionLimit = 10;
 
@@ -21,10 +22,17 @@ exports.query = async function (sql, args) {
         }
     }
 
+    function mapArg(value) {
+        if (value && value.getUTCFullYear) {
+            return dateFormat(value, 'yyyy-mm-dd HH:MM:ss');
+        }
+        return value;
+    }
+
     return new Promise(function (resolve, reject) {
         pool.query(
             sql.replace(regex, "?"),
-            params.map(x => args[x]),
+            params.map(x => mapArg(args[x])),
             function (err, rows, fields) {
                 if (err) reject(err);
                 resolve(rows);
