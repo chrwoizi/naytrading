a021140e03a282540425be5ec483f44c355e6a0e
chrwoizi <christian@woizischke.de>
2018-09-09 23:52:05 +0200

    Fix raspberry install script

--- NAYtrading.AI/install.sh
+++ NAYtrading.AI/install.sh
@@ -1,14 +1,4 @@
 #!/bin/sh
-sudo apt-get -y install libssl-dev openssl
-sudo apt-get -y install build-essential checkinstall
-sudo apt-get -y install libreadline-gplv2-dev libncursesw5-dev libssl-dev openssl libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libpq-dev zlib1g-dev
-sudo apt-get -y install libssl1.0-dev
-export CFLAGS="-I/usr/local/opt/openssl/include -L/usr/local/opt/openssl/lib -I/usr/local/opt/zlib/include -L/usr/local/opt/zlib/lib"
-wget https://www.python.org/ftp/python/3.4.9/Python-3.4.9.tar.xz
-tar xf Python-3.4.9.tar.xz
-cd Python-3.4.9
-./configure
-make -j4
-sudo make altinstall
-sudo pip3.4 install requests
-sudo pip3.4 install tensorflow
+sudo apt-get -y install libatlas-base-dev
+sudo pip3 install requests
+sudo pip3 install https://www.piwheels.org/simple/tensorflow/tensorflow-1.9.0-cp35-none-linux_armv7l.whl#sha256=d47210ed7608d4caa9716d29a2fa774a4a64e598cc82cc2df4f5b85a16b1f813
