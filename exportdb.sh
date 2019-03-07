3436b6353260111d635d17c6cbebfef9a2c2e75d
chrwoizi <christian@woizischke.de>
2019-03-07 18:02:40 +0000

    add export script

---
+++ exportdb.sh
@@ -0,0 +1,19 @@
+dbuser=naytrading
+dbpass=naytrading
+dbname=naytrading
+rm -r dump
+mkdir dump
+cd dump
+mysqldump --user="$dbuser" --password="$dbpass" "$dbname" instruments > instruments.sql
+mysqldump --user="$dbuser" --password="$dbpass" "$dbname" portfolios > portfolios.sql
+mysqldump --user="$dbuser" --password="$dbpass" "$dbname" settings > settings.sql
+mysqldump --user="$dbuser" --password="$dbpass" "$dbname" snapshots > snapshots.sql
+mysqldump --user="$dbuser" --password="$dbpass" "$dbname" snapshotrates > snapshotrates.sql
+mysqldump --user="$dbuser" --password="$dbpass" "$dbname" sources > sources.sql
+mysqldump --user="$dbuser" --password="$dbpass" "$dbname" trades > trades.sql
+mysqldump --user="$dbuser" --password="$dbpass" "$dbname" tradelogs > tradelogs.sql
+tar czf dump.tar.gz *
+rm *.sql
+mv -f dump.tar.gz ../
+cd ..
+rmdir dump
