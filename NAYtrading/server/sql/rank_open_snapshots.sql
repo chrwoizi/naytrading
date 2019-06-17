a7de4c52924df5628e083735355cfa1c3a096037
chrwoizi <christian@woizischke.de>
2019-06-17 19:49:09 +0000

    Improve performance on new snapshot

--- NAYtrading/server/sql/rank_open_snapshots.sql
+++ NAYtrading/server/sql/rank_open_snapshots.sql
@@ -27,17 +27,6 @@ AND NOT EXISTS
 	AND u.User = @userName
 ) 
 AND s.Time >= NOW() - INTERVAL @hours HOUR
-AND CASE (
-		SELECT su.Decision 
-		FROM usersnapshots AS su 
-        INNER JOIN snapshots AS sus ON sus.ID = su.Snapshot_ID
-		WHERE sus.Instrument_ID = s.Instrument_ID AND su.User = @userName
-        ORDER BY sus.Time DESC LIMIT 1
-	)
-	WHEN 'wait1yr' THEN s.Time < NOW() - INTERVAL 1 YEAR 
-	WHEN 'wait2mo' THEN s.Time < NOW() - INTERVAL 2 MONTH 
-    ELSE TRUE 
-    END
 UNION ALL
 SELECT 
 	1 AS source, 
@@ -68,17 +57,6 @@ AND NOT EXISTS
 	AND u.User = @userName
 ) 
 AND s.Time < NOW() - INTERVAL @hours HOUR 
-AND CASE (
-		SELECT su.Decision 
-		FROM usersnapshots AS su 
-        INNER JOIN snapshots AS sus ON sus.ID = su.Snapshot_ID
-		WHERE sus.Instrument_ID = s.Instrument_ID AND su.User = @userName
-        ORDER BY su.ModifiedTime DESC LIMIT 1
-	)
-	WHEN 'wait1yr' THEN s.Time < NOW() - INTERVAL 1 YEAR 
-	WHEN 'wait2mo' THEN s.Time < NOW() - INTERVAL 2 MONTH 
-    ELSE TRUE 
-    END
 ORDER BY 
 	source ASC, 
 	ageOrder ASC, 
