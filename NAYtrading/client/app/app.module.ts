8e9e77499c570c1c01d3cbc8d192d58b22b18ec6
chrwoizi <christian@woizischke.de>
2020-08-12 17:30:02 +0000

    upgrade to angular 10

--- NAYtrading/client/app/app.module.ts
+++ NAYtrading/client/app/app.module.ts
@@ -1,37 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { ContactComponent } from './contact/contact.component';
-import { RegisterComponent } from './register/register.component';
-import { LoginComponent } from './login/login.component';
-import { PasswordComponent } from './password/password.component';
-import { ManageComponent } from './manage/manage.component';
-import { FaqComponent } from './faq/faq.component';
-import { TermsComponent } from './terms/terms.component';
-import { CookiesComponent } from './cookies/cookies.component';
-import { DisclaimerComponent } from './disclaimer/disclaimer.component';
-import { PrivacyComponent } from './privacy/privacy.component';
-import { StatsComponent } from './stats/stats.component';
-import { InstrumentsComponent } from './instruments/instruments.component';
-import { SnapshotComponent } from './snapshot/snapshot.component';
-import { SnapshotsComponent } from './snapshots/snapshots.component';
-import { SuggestionsComponent } from './suggestions/suggestions.component';
-import { SuggestionComponent } from './suggestion/suggestion.component';
-import { AdminComponent } from './admin/admin.component';
-import { MonitorComponent } from './monitor/monitor.component';
-import { WhitelistComponent } from './whitelist/whitelist.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+import { ContactComponent } from "./contact/contact.component";
+import { RegisterComponent } from "./register/register.component";
+import { LoginComponent } from "./login/login.component";
+import { PasswordComponent } from "./password/password.component";
+import { ManageComponent } from "./manage/manage.component";
+import { FaqComponent } from "./faq/faq.component";
+import { TermsComponent } from "./terms/terms.component";
+import { CookiesComponent } from "./cookies/cookies.component";
+import { DisclaimerComponent } from "./disclaimer/disclaimer.component";
+import { PrivacyComponent } from "./privacy/privacy.component";
+import { StatsComponent } from "./stats/stats.component";
+import { InstrumentsComponent } from "./instruments/instruments.component";
+import { SnapshotComponent } from "./snapshot/snapshot.component";
+import { SnapshotsComponent } from "./snapshots/snapshots.component";
+import { SuggestionsComponent } from "./suggestions/suggestions.component";
+import { SuggestionComponent } from "./suggestion/suggestion.component";
+import { AdminComponent } from "./admin/admin.component";
+import { MonitorComponent } from "./monitor/monitor.component";
+import { WhitelistComponent } from "./whitelist/whitelist.component";
 
-import { AlertComponent } from './alert/alert.component';
-import { JwtInterceptor } from './helpers/jwt.interceptor';
-import { ErrorInterceptor } from './helpers/error.interceptor';
-import { InfiniteScrollModule } from 'ngx-infinite-scroll';
-import { NgxSpinnerModule } from 'ngx-spinner';
-import { ChartsModule } from 'ng2-charts';
+import { AlertComponent } from "./alert/alert.component";
+import { JwtInterceptor } from "./helpers/jwt.interceptor";
+import { ErrorInterceptor } from "./helpers/error.interceptor";
+import { InfiniteScrollModule } from "ngx-infinite-scroll";
+import { NgxSpinnerModule } from "ngx-spinner";
+import { ChartsModule } from "ng2-charts";
 
 @NgModule({
   declarations: [
@@ -56,7 +56,7 @@ import { ChartsModule } from 'ng2-charts';
     SuggestionComponent,
     AdminComponent,
     MonitorComponent,
-    WhitelistComponent
+    WhitelistComponent,
   ],
   imports: [
     BrowserModule,
@@ -66,12 +66,12 @@ import { ChartsModule } from 'ng2-charts';
     AppRoutingModule,
     InfiniteScrollModule,
     NgxSpinnerModule,
-    ChartsModule
+    ChartsModule,
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
-    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true }
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
